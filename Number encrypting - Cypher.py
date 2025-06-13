6567f8c5209c5b2a55bbe9ad52c71dd2dd2c5eb6
anjom <machatschekanjo@gmail.com>
2025-06-13 16:29:54 +0200

    Fix cypher function to correctly create translation dictionary using str.maketrans

--- Number encrypting - Cypher.py
+++ Number encrypting - Cypher.py
@@ -1,7 +1,7 @@
 def cypher(s):
     nums = [str(x) for x in range(10)]
     ltrs = [("O", "o"), ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B",), ("g",)]
-    transl = dict(zip(nums, ltrs))
+    transl = str.maketrans(zip(ltrs, nums))
     
     return s, transl
 
