5968dd79cbb01fb75173766afc8acf12a683dcbe
anjom <machatschekanjo@gmail.com>
2025-06-13 16:32:32 +0200

    Fix cypher function to correctly create translation table and return translated string

--- Number encrypting - Cypher.py
+++ Number encrypting - Cypher.py
@@ -1,9 +1,9 @@
 def cypher(s):
     nums = [str(x) for x in range(10)]
-    ltrs = [("O", "o"), ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B",), ("g",)]
-    transl = str.maketrans(zip(ltrs, nums))
-    
-    return s, transl
+    ltrs = [("O", "o"), ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B", None), ("g", None)]
+    table = str.maketrans(zip(ltrs, nums))
+    transl = s.translate(table) 
+    return transl
 
 
 print(cypher("Hello World"))
