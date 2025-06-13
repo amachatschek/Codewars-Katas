0ad1bc6a2b1d65f8091744b032a8fbfcd20e4c2a
anjom <machatschekanjo@gmail.com>
2025-06-13 21:23:45 +0200

    Fix indexing errors in number function and add loop for processing bus stops

--- People on the Bus.py
+++ People on the Bus.py
@@ -1,5 +1,10 @@
 def number(bus_stops):
-    
-    # Good Luck!
+    arr = bus_stops
+    inside = arr[1[1]]
+    out = arr[1[2]]
 
-number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]])
+    for i in arr:
+        
+        
+
+print(number([[3,0],[9,1],[4,10],[12,2],[6,1],[7,10]]))
