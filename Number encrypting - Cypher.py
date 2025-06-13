bf966a2860e1838973eeb59a003d57e4e73b6585
anjom <machatschekanjo@gmail.com>
2025-06-13 17:24:04 +0200

    Fix cypher function to correct letter mappings and update translation logic

--- Number encrypting - Cypher.py
+++ Number encrypting - Cypher.py
@@ -1,9 +1,13 @@
 def cypher(s):
     nums = [str(x) for x in range(10)]
-    ltrs = [("O", "o"), ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B", None), ("g", None)]
-    table = str.maketrans(zip(ltrs, nums))
-    transl = s.translate(table) 
-    return transl
+    ltrs = {"O", "o" ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B",), ("g",)}
+    for i in ltrs:  
+        a, b = i
+
+    #transl = s.translate(table) 
+    transl = dict.fromkeys(ltrs, nums)
+    print(ltrs)
+    return ltrs
 
 
 print(cypher("Hello World"))
