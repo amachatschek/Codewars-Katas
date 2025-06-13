0a598f022f694e0631e110d7678c3cc0a4569820
anjom <machatschekanjo@gmail.com>
2025-06-13 04:14:02 +0200

    Fix filter_list function to correctly filter out non-integer values

--- List Filtering.py
+++ List Filtering.py
@@ -1,6 +1,7 @@
 def filter_list(l):
     'return a new list with the strings filtered out'
-    lc = [type ]
+    lc = [x for x in l if type(x) == int]
+    return lc
 
 
-filter_list([1,'a','b',0,15]) # == [1,0,15]
+print(filter_list([1,'a','b',0,15])) # == [1,0,15]
