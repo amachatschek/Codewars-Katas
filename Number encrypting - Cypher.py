1fda9b6b693b0f441207b70529f4e470f2f225b8
anjom <machatschekanjo@gmail.com>
2025-06-13 06:07:12 +0200

    Refactor cypher function to iterate over letter mappings

--- Number encrypting - Cypher.py
+++ Number encrypting - Cypher.py
@@ -2,6 +2,7 @@ def cypher(s):
     nums = [x for x in range(10)]
     ltrs = [("O", "o"), ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B",), ("g",)]
     transl = dict(zip(ltrs, nums))
-    return transl, s
+    for o, l in ltrs.items():
+        
 
 print(cypher("Hello World"))
