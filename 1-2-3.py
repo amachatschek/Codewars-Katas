4c75a601ede97a92535c09d2da2cd08ad6269dcd
anjom <machatschekanjo@gmail.com>
2025-06-13 20:09:07 +0200

    Refactor number function to use list comprehension for creating new_list

--- 1-2-3.py
+++ 1-2-3.py
@@ -1,7 +1,7 @@
 def number(lines):
-    new_list = []
-    for i, item in enumerate(lines, 1):
-        new_list.append(f"{i}: {item}")    
+    new_list = [f"{i}: {item}" for i, item in enumerate(lines, 1)]
+    #for i, item in enumerate(lines, 1):
+    #    new_list.append(f"{i}: {item}")    
     return new_list
 
 print(number(["a", "b", "c"]))
