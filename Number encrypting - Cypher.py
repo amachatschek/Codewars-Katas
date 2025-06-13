d5cb60b928e8334f9f29c68e6620b6e4e543da89
anjom <machatschekanjo@gmail.com>
2025-06-13 05:48:50 +0200

    quicksave

---
+++ Number encrypting - Cypher.py
@@ -0,0 +1,4 @@
+def cypher(s):
+    nums = [x for x in range(10)]
+    ltrs = [("O", "o"), ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B",), ("g",)]
+    transl = 
