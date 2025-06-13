6dd5c8f40a6fea4e086ac7d7f549cfc5a1df7148
anjom <machatschekanjo@gmail.com>
2025-06-13 20:04:41 +0200

    first edition

--- 1-2-3.py
+++ 1-2-3.py
@@ -1,5 +1,7 @@
 def number(lines):
     new_list = []
     for i, item in enumerate(lines, 1):
-        new_list.append(f"")    
-    return f""
+        new_list.append(f"{i}: {item}")    
+    return new_list
+
+print(number(["a", "b", "c"]))
