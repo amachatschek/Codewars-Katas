2170f3d3617b48f3693b45a08b76df0caaaa68a9
anjom <machatschekanjo@gmail.com>
2025-06-13 05:55:51 +0200

    Implement cypher function to create a translation dictionary and return input string

--- Number encrypting - Cypher.py
+++ Number encrypting - Cypher.py
@@ -1,4 +1,7 @@
 def cypher(s):
     nums = [x for x in range(10)]
     ltrs = [("O", "o"), ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B",), ("g",)]
-    transl = 
+    transl = dict(zip(ltrs, nums))
+    return transl, s
+
+print(cypher("Hello World"))
