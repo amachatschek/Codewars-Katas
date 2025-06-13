150a4ec06671365bbf46c716bd0dbbcde64dfd45
anjom <machatschekanjo@gmail.com>
2025-06-13 20:35:58 +0200

    Fix syntax error in cypher function by removing extraneous brace

--- Number encrypting - Cypher.py
+++ Number encrypting - Cypher.py
@@ -1,7 +1,6 @@
 def cypher(s):
     nums = [str(x) for x in range(10)]
     ltrs = ["O", "o",  "I", "l", "R", "z", "E", "e", "A", "a", "S", "s", "G", "b", "T", "t", "B", "g"]
-            }
     for i in ltrs:  
         a, b = i
 
