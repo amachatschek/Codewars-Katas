b6da385edb3849fb855475baf89d3023d243b023
anjom <machatschekanjo@gmail.com>
2025-06-13 18:06:35 +0200

    Fix cypher function to correct letter list initialization and structure

--- Number encrypting - Cypher.py
+++ Number encrypting - Cypher.py
@@ -1,6 +1,7 @@
 def cypher(s):
     nums = [str(x) for x in range(10)]
-    ltrs = {"O", "o" ("I", "l"), ("R", "z"), ("E", "e"), ("A", "a"), ("S", "s"), ("G", "b"), ("T", "t"), ("B",), ("g",)}
+    ltrs = ["O", "o",  "I", "l", "R", "z", "E", "e", "A", "a", "S", "s", "G", "b", "T", "t", "B", "g"]
+            }
     for i in ltrs:  
         a, b = i
 
