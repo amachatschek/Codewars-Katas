b7af2be227e213ae45fef84f9a7dc1f286d96aa7
anjom <machatschekanjo@gmail.com>
2025-06-23 01:00:48 +0200

    finished

--- Multiply Word in String.py
+++ Multiply Word in String.py
@@ -1,6 +1,5 @@
 def modify_multiply(st, loc, num):
-    return st.split()[loc] 
-
+    return str((st.split()[loc] + "-") * num).rstrip("-")
 
 
 print(modify_multiply("Creativity is the process of having original ideas that have value. It is a process; it's not random.",8 ,10))
